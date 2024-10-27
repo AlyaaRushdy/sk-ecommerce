0a8fe8a9761fe0fbda95afc33bb1266ade2c72b5
MennaAllahYasser11 <mennaallahyasser111@gmail.com>
2024-10-27 23:59:29 +0300

    add route for product

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/shared/Navbar";
 import Home from "./pages/Home";
+import Product from "./components/products/Product"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product/>} />
       </Routes>
     </>
   );
