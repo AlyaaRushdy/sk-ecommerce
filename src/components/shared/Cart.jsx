8b4f1706755f621f24e70a05dd94f6a622fc9321
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-30 08:09:06 +0300

    made an emptyCart component and added scroll area to cart and made minor styling changes

--- src/components/shared/Cart.jsx
+++ src/components/shared/Cart.jsx
@@ -11,6 +11,8 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 import CartItem from "../cart/CartItem";
+import EmptyCart from "../cart/emptyCart";
+import { ScrollArea } from "../ui/scroll-area";
 
 function Cart() {
   const [products, setProducts] = useState([]);
@@ -32,26 +34,32 @@ function Cart() {
         <SheetTrigger asChild>
           <Button variant="outline">Cart</Button>
         </SheetTrigger>
-        <SheetContent className="w-5/6 max-w-96 px-4 sm:px-6">
-          <SheetHeader className=" text-left pb-2">
+        <SheetContent className="w-11/12 max-w-96 px-0 flex flex-col gap-0">
+          <SheetHeader className="border-b text-left px-4 sm:px-6 pb-2">
             <SheetTitle>CART</SheetTitle>
           </SheetHeader>
-          {products.length > 0 && (
-            <div className="flex flex-col gap-3 py-4">
-              {products.map((product, index) => (
-                <CartItem
-                  product={product}
-                  isLast={products.length - 1 == index}
-                  key={index}
-                />
-              ))}
-            </div>
+          {products.length == 0 ? (
+            <EmptyCart />
+          ) : (
+            <>
+              <ScrollArea className="px-4 sm:px-6 mr-2 flex-grow">
+                <div className="flex flex-col gap-3 py-4 ">
+                  {products.map((product, index) => (
+                    <CartItem
+                      product={product}
+                      isLast={products.length - 1 == index}
+                      key={index}
+                    />
+                  ))}
+                </div>
+              </ScrollArea>
+              <SheetFooter className={"px-4 sm:px-6"}>
+                <SheetClose asChild>
+                  <Button className={"sm:w-full"}>Check Out</Button>
+                </SheetClose>
+              </SheetFooter>
+            </>
           )}
-          <SheetFooter>
-            <SheetClose asChild>
-              <Button className={"sm:w-full"}>Check Out</Button>
-            </SheetClose>
-          </SheetFooter>
         </SheetContent>
       </Sheet>
     </>
