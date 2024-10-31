febf84e20071bbef2ad37ba68d363712a6133ab6
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-31 03:30:27 +0300

    added toaster in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Product from "./components/products/Product";
 import Cart from "./components/shared/Cart";
 import Products from "./pages/Products";
 import Navbar from "./components/shared/Navbar";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/products" element={<Products />} />
       </Routes>
       <Cart />
+      <Toaster />
     </>
   );
 }
