d27bbd1a650aa1b5c36ee2d7a57e500974479704
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-31 03:32:03 +0300

    add product from Products to cart

--- src/components/products/productItem.jsx
+++ src/components/products/productItem.jsx
@@ -1,27 +1,39 @@
 /* eslint-disable react/prop-types */
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { add } from "@/slices/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function ProductItem({ product }) {
+  const { productsIds } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+
   const priceAfterDiscount = (originalPrice, discountPercentage) => {
     const discountedAmount = (originalPrice * discountPercentage) / 100;
     return originalPrice - discountedAmount;
   };
 
+  const handleAddToCart = () => {
+    dispatch(add({ ...product, quantity: 1 }));
+    toast({
+      description: `${product.title} added to cart successfully!`,
+    });
+  };
+
   return (
     <>
-      <div className="relative rounded hover:border-y-2 hover:-my-0.5 border-primary transition-all duration-100 group">
+      <div className="relative rounded border-y-2 border-y-transparent hover:border-y-primary transition-all group">
         <Link to={`/product/${product.id}`}>
           <img
             src="/src/assets/HydratingOil.jpg"
             alt={`${product.title} image`}
-            className="aspect-square w-full object-cover "
+            className="aspect-square w-full object-cover"
           />
         </Link>
-        <div className="opacity-100 group-hover:opacity-0 transition-all">
-          <Link to={`/product/${product.id}`}>
-            <h2 className="text-lg sm:text-xl py-2">{product.title}</h2>
-          </Link>
+        <div className="opacity-100 visible group-hover:opacity-0 group-hover:invisible transition-all">
+          <h2 className="text-lg sm:text-xl py-2">{product.title}</h2>
           <p>
             {priceAfterDiscount(product.basePrice, product.discountPercentage)}{" "}
             EGP
@@ -32,19 +44,18 @@ function ProductItem({ product }) {
             )}
           </p>
         </div>
-        <div className="text-center opacity-0 group-hover:opacity-100 transition-all">
-          <Button className="absolute bottom-3 -translate-x-1/2">
-            Add To Cart
-            <span className="hidden sm:inline-block">
-              -{" "}
-              {priceAfterDiscount(
-                product.basePrice,
-                product.discountPercentage
-              )}{" "}
-              EGP
-            </span>
-          </Button>
-        </div>
+        <Button
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 opacity-0 invisible group-hover:opacity-100 group-hover:visible group-hover:-translate-y-3 group-hover:disabled:opacity-50 transition-all"
+          onClick={handleAddToCart}
+          disabled={productsIds.includes(product.id) ? true : false}
+        >
+          Add To Cart
+          <span className="hidden sm:inline-block">
+            -{" "}
+            {priceAfterDiscount(product.basePrice, product.discountPercentage)}{" "}
+            EGP
+          </span>
+        </Button>
       </div>
     </>
   );
