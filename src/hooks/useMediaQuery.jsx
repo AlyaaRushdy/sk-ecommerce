dbb5f15c81190154e8248d1057c1e21cfd4d4062
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-11-01 08:59:47 +0200

    useMediaQuery custom hook

---
+++ src/hooks/useMediaQuery.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function useMediaQuery(query) {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    const updateMatch = () => setMatches(mediaQueryList.matches);
+
+    mediaQueryList.addEventListener("change", updateMatch);
+    return () => mediaQueryList.removeEventListener("change", updateMatch);
+  }, [query]);
+
+  return matches;
+}
+export default useMediaQuery;
