321c416b2bb62e38e5ea8201c3e67dc3ba084398
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-11-07 14:36:15 +0200

    login and registeration pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Cart from "./components/shared/Cart";
-import Products from "./pages/Products";
 import Navbar from "./components/shared/Navbar";
+import Cart from "./components/shared/Cart";
 import { Toaster } from "./components/ui/toaster";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import RefundPolicy from "./pages/RefundPolicy";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/register";
+
+const productsPaths = [
+  {
+    path: "/bodyOils",
+    url: "http://localhost:5000/categories/code/bodyOils",
+    h1Text: "Body Oils",
+  },
+  {
+    path: "/masks",
+    url: "http://localhost:5000/categories/code/masks",
+    h1Text: "Masks",
+  },
+  {
+    path: "/allproducts",
+    url: "http://localhost:5000/products/",
+    h1Text: "All Products",
+  },
+];
 
 function App() {
   return (
@@ -14,38 +34,20 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
+        {productsPaths.map((route, i) => {
+          return (
+            <Route
+              path={route.path}
+              element={<Products h1Text={route.h1Text} url={route.url} />}
+              key={i}
+            />
+          );
+        })}
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/refundpolicy" element={<RefundPolicy />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/contact" element={<Contact />} />
-
-        <Route
-          path="/bodyOils"
-          element={
-            <Products
-              url={"http://localhost:5000/categories/code/bodyOils"}
-              h1Text="Body Oils"
-            />
-          }
-        />
-        <Route
-          path="/masks"
-          element={
-            <Products
-              url={"http://localhost:5000/categories/code/masks"}
-              h1Text="Masks"
-            />
-          }
-        />
-        <Route
-          path="/allproducts"
-          element={
-            <Products
-              url={"http://localhost:5000/products/"}
-              h1Text="All Products"
-            />
-          }
-        />
       </Routes>
       <Cart />
       <Toaster />
